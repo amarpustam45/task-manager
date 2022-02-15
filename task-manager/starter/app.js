ac288aa8244ab83c719323b854e3534f3a479ab4
amarpustam45 <amarpustam@hotmail.com>
2022-02-15 15:35:10 -0400

    added the mongoDB to persist data

--- task-manager/starter/app.js
+++ task-manager/starter/app.js
@@ -1,1 +1,33 @@
-console.log('Task Manager App')
+const express = require('express');
+const app = express();
+const port = 3000;
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+require('dotenv').config();
+
+//middleware
+app.use(express.json());
+
+//routes
+app.get('/', (req, res) => {
+  res.send('task manager app');
+});
+
+app.use('/api/v1/tasks', tasks);
+
+// app.get('/api/v1/task', (req, res) => {});
+// app.post('/api/v1/task', (req, res) => {});
+// app.patch('/api/v1/task/:id', (req, res) => {});
+// app.delete('/api/v1/task/:id', (req, res) => {});
+// app.get('/api/v1/task/:id', (req, res) => {});
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(3000, console.log(`server is listening on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
---
+++ task-manager/starter/controllers/tasks.js
@@ -0,0 +1,23 @@
+const Task = require('../models/task');
+
+const getAllTasks = (req, res) => {
+  res.send('all items from controllers');
+};
+
+const createTask = async (req, res) => {
+  const task = await Task.create(req.body);
+  res.status(201).json({ task });
+};
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id });
+};
+
+const updateTask = (req, res) => {
+  res.send('update single task');
+};
+
+const deleteTask = (req, res) => {
+  res.send('delete single task');
+};
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
